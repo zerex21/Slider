224a0d85a13c7baed448eb3b1630139e2dcb5aec
oleg <58697249+zerex21@users.noreply.github.com>
2021-10-16 18:12:29 +0300

    feat:add js (Sat, Oct 16, 2021 18:12:32 PM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -26,3 +26,42 @@ const activeText = n =>{
     }
     texts[n].classList.add('active')
 }  
+const prepareCurrentSlide = ind =>{
+    activeSlide(ind);
+    activeDot(ind);
+}
+const nextSlide = () =>{
+    if(index == slides.length - 1){
+        index = 0;
+        prepareCurrentSlide(index)
+    }else{
+        index++;
+        prepareCurrentSlide(index)
+    }
+}
+
+const prevSlide = () =>{
+    if(index == 0){
+        index = slides.length - 1;
+        prepareCurrentSlide(index)
+    }else{
+        index--;
+        prepareCurrentSlide(index)
+    }
+}
+
+dots.forEach((item, indexDot)=>{
+  item.addEventListener('click', ()=>{
+      index = indexDot
+      prepareCurrentSlide(index)
+      activeText(index)
+  })
+})
+
+dotsWrapper.forEach((item, indexDot)=>{
+    item.addEventListener('click', ()=>{
+        index = indexDot
+        prepareCurrentSlide(index)
+        activeText(index)
+    })
+  })
